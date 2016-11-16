51d85cadf0799f34f901878f0bca40f7866b40ea
Mike Luz <onlymikeluz@gmail.com>
2016-11-16 17:32:16 -0500

    updated PD track

---
+++ music.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- head -->
+<head>
+  <title>Mercy Wizard | Dev Music Ideas</title>
+  <meta charset="utf-8">
+<meta name=viewport content='width=700'>
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <script src="accordion.js"></script>
+  <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="MWsite.css" type="text/css" media="screen"/>
+</head>
+
+<!-- body -->
+<body>
+
+	<!-- banner -->
+	<div class="banner">
+ 	  <a href="index.php"><img src="images/handwrittenbanner.jpg" alt="MERCY WIZARD" height="120" width=auto></a>	
+ 	</div>
+
+
+<!-- social media sidebar -->	
+<aside id="sticky-social">
+    <ul>
+        <li><a href="https://www.facebook.com/Mercy-Wizard-138067332877122/" class="entypo-facebook" target="_blank"><span>Facebook</span></a></li>
+        <li><a href="https://twitter.com/mercywizard" class="entypo-twitter" target="_blank"><span>Twitter</span></a></li>
+        <li><a href="https://www.linkedin.com/in/mike-luz-a6714497" class="entypo-linkedin" target="_blank"><span>LinkedIn</span></a></li>
+        <li><a href="https://www.instagram.com/mercywizard/" class="entypo-instagrem" target="_blank"><span>Instagram</span></a></li>
+    </ul>
+</aside>
+
+	<!-- menu -->
+	<br>
+	<ul class="menu">
+	<li><a href="dev.php">Dev</a></li>
+	<li> / </li>
+	<li><a href="photos.php">Photos</a></li>	
+	<li> / </li>
+	<li><a href="/blog">Blog</a></li>
+	</ul>
+	<br>
+	
+<hr>
+	<!-- music -->
+	<div id="music">
+				
+<iframe style="border: 0; width: 350px; height: 350px;" src="http://bandcamp.com/EmbeddedPlayer/album=2558910622/size=large/bgcol=ffffff/linkcol=0687f5/minimal=true/transparent=true/" seamless><a href="http://mercywizard.bandcamp.com/album/dream-awake">Dream Awake by Mercy Wizard</a></iframe>
+
+<iframe style="border: 0; width: 350px; height: 350px;" src="http://bandcamp.com/EmbeddedPlayer/album=3558591457/size=large/bgcol=ffffff/linkcol=0687f5/minimal=true/transparent=true/" seamless><a href="http://pescidevito.bandcamp.com/album/drunk-on-gas">Drunk On Gas by Pesci Devito</a></iframe>
+
+<iframe style="border: 0; width: 350px; height: 350px;" src="https://bandcamp.com/EmbeddedPlayer/album=3875440212/size=large/bgcol=ffffff/linkcol=0687f5/minimal=true/transparent=true/" seamless><a href="http://theimmaculatecorpses.bandcamp.com/album/the-tape">The Tape by Immaculate Corpses</a></iframe>
+
+<iframe style="border: 0; width: 350px; height: 350px;" src="http://bandcamp.com/EmbeddedPlayer/album=3025760993/size=large/bgcol=ffffff/linkcol=0687f5/minimal=true/transparent=true/" seamless><a href="http://disunicorps.bandcamp.com/album/sobotomy">Sobotomy by Disunicorps</a></iframe>
+
+<iframe style="border: 0; width: 350px; height: 350px;" src="https://bandcamp.com/EmbeddedPlayer/album=856873363/size=large/bgcol=ffffff/linkcol=0687f5/minimal=true/transparent=true/" seamless><a href="http://fulpruf.bandcamp.com/album/01">01 by ful.pruf</a></iframe>
+
+<iframe style="border: 0; width: 350px; height: 350px;" src="http://bandcamp.com/EmbeddedPlayer/album=1627368487/size=large/bgcol=ffffff/linkcol=0687f5/minimal=true/transparent=true/" seamless><a href="http://howlingboil.bandcamp.com/album/maiden-america">Maiden America by Howling Boil</a></iframe>
+
+<iframe style="border: 0; width: 350px; height: 350px;" src="http://bandcamp.com/EmbeddedPlayer/album=2187746142/size=large/bgcol=ffffff/linkcol=0687f5/minimal=true/transparent=true/" seamless><a href="http://groupdynamics.bandcamp.com/album/illegal-hours">Illegal Hours by Group Dynamics</a></iframe>
+
+<iframe style="border: 0; width: 350px; height: 350px;" src="http://bandcamp.com/EmbeddedPlayer/album=2820214114/size=large/bgcol=ffffff/linkcol=0687f5/minimal=true/transparent=true/" seamless><a href="http://futurenoisemusic.bandcamp.com/album/monitor">Monitor by Future Noise</a></iframe>
+
+<iframe style="border: 0; width: 350px; height: 350px;" src="http://bandcamp.com/EmbeddedPlayer/album=3167630631/size=large/bgcol=ffffff/linkcol=0687f5/minimal=true/transparent=true/" seamless><a href="http://luzagnew.bandcamp.com/album/identical-mistakes">identical mistakes by Luz &amp; Agnew</a></iframe>
+
+<iframe style="border: 0; width: 350px; height: 350px;" src="http://bandcamp.com/EmbeddedPlayer/album=656885489/size=large/bgcol=ffffff/linkcol=0687f5/minimal=true/transparent=true/" seamless><a href="http://thestorageunit.bandcamp.com/album/ornate-space-gorilla">ornate space gorilla by the storage unit</a></iframe>
+
+<iframe style="border: 0; width: 350px; height: 350px;" src="https://bandcamp.com/EmbeddedPlayer/album=1338458338/size=large/bgcol=ffffff/linkcol=0687f5/minimal=true/transparent=true/" seamless><a href="http://weirdpony.bandcamp.com/album/automotionation">Automotionation by weird pony</a></iframe>
+
+	</div>  
+	
+<hr>
+
+	<!-- footer -->
+	<ul class="menu2">
+	<li><a href="about.php">About</a></li>
+	<li>  /  </li>
+	<li><a href="mailto:onlymikeluz@gmail.com">Contact</a></li>
+	<li>  /  </li>
+	<li><a href="http://www.beingrecords.com/">Being Records</a></li>
+	</ul>
+	
+	<br>
+	
+  		<div>   
+ 		<?php include "./includes/copyright.php" ?>
+                </div>
+	<br>
+
+</body>
+</html>
